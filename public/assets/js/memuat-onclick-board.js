0d8a004cf64cd2fba931c1b35d56d43324b6b2fd
Kelvin <kelvin.p2504@gmail.com>
2024-07-02 01:04:03 +0700

    Callback onclick

--- public/assets/js/memuat-onclick-board.js
+++ public/assets/js/memuat-onclick-board.js
@@ -111,7 +111,6 @@ function changeCard(id) {
             if (modalTarget !== "-- Select Card --") {
                 $('.modal').modal('hide');
                 setTimeout(function() {
-                    $(modalTarget).modal('show');
                     $('#isianKartu').modal('show');
                     $('#card_id').val(modalTarget);
                     $('#form_kartu').submit();
