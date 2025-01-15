45016497e6b093f86b896239f5b61be6b9ee1d2c
SatrioBagasPangestu <navi.squishy.23@gmail.com>
2025-01-15 11:44:27 +0700

    🔧 Fix: Bug posisiton copied card

--- app/Http/Controllers/CopyCardController.php
+++ app/Http/Controllers/CopyCardController.php
@@ -2,126 +2,142 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use Carbon\Carbon;
 use App\Models\Card;
 use App\Models\Column;
-use App\Models\TitleChecklists;
 use App\Models\Checklists;
 use App\Models\ModeAplikasi;
-use Carbon\Carbon;
+use Illuminate\Http\Request;
+use App\Models\TitleChecklists;
+use Illuminate\Support\Facades\DB;
 
 class CopyCardController extends Controller
 {
     // Untuk menyalin kartu, judul ceklist dan ceklist
     public function copyCard(Request $request, $column_id, $id)
     {
-        // Validasi inputan
-        $request->validate([
-            'name' => 'required|string|max:255',
-        ]);
+        // $request->dd();
+        try{
+            DB::beginTransaction();
 
-        // Temukan kartu asli atau tiruan
-        $card = Card::findOrFail($id);
+             // Validasi inputan
+            $request->validate([
+                'name' => 'required|string|max:255',
+            ]);
 
-        // Tentukan posisi baru
-        $newPosition = $card->position != 0 ? $card->position + 1 : 0;
+            // Temukan kartu asli atau tiruan
+            $card = Card::findOrFail($id);
 
-        // Perbarui posisi kartu lain di kolom yang sama
-        Card::where('column_id', $column_id)
-            ->where('position', '>=', $newPosition)
-            ->increment('position');
+            // Tentukan posisi baru
+            // $newPosition = $card->position != 0 ? $card->position + 1 : 0;
 
-        // Salin Kartu
-        $newCard = $card->replicate();
-        $newCard->name = $request->input('name');
-        $newCard->position = $newPosition;
-        $newCard->created_at = Carbon::now();
-        $newCard->save();
+            // posisi card yang baru di copy akan diletakkan di paling bawah
+            $newPosition = Card::where('column_id', $column_id)->max('position') + 1;
 
-        $checklists = [];
-        // dd($request->keep_checklists);
-        if ($request->input('keep_checklists')) {
-            $titleChecklists = TitleChecklists::where('cards_id', $id)->get();
+            // Perbarui posisi kartu lain di kolom yang sama
+            Card::where('column_id', $column_id)
+                ->where('position', '>=', $newPosition)
+                ->increment('position');
+
+            // Salin Kartu
+            $newCard = $card->replicate();
+            $newCard->name = $request->input('name');
+            $newCard->position = $newPosition;
+            $newCard->created_at = Carbon::now();
+            $newCard->save();
+
             $checklists = [];
-            foreach ($titleChecklists as $titleChecklist) {
-                $newTitleChecklist = $titleChecklist->replicate();
-                $newTitleChecklist->cards_id = $newCard->id;
-                $newTitleChecklist->created_at = Carbon::now();
+            // dd($request->keep_checklists);
+            if ($request->input('keep_checklists')) {
+                $titleChecklists = TitleChecklists::where('cards_id', $id)->get();
+                $checklists = [];
+                foreach ($titleChecklists as $titleChecklist) {
+                    $newTitleChecklist = $titleChecklist->replicate();
+                    $newTitleChecklist->cards_id = $newCard->id;
+                    $newTitleChecklist->created_at = Carbon::now();
 
-                // Jika checkbox tidak tercentang, set nilai percentage
-                if (!$request->input('keep_checklists')) {
-                    $newTitleChecklist->percentage = NULL;
-                }
+                    // Jika checkbox tidak tercentang, set nilai percentage
+                    if (!$request->input('keep_checklists')) {
+                        $newTitleChecklist->percentage = NULL;
+                    }
 
-                $newTitleChecklist->save();
+                    $newTitleChecklist->save();
 
-                // Salin Ceklist
-                $checklists = Checklists::where('title_checklists_id', $titleChecklist->id)->get();
-                foreach ($checklists as $checklist) {
-                    $newChecklist = $checklist->replicate();
-                    $newChecklist->title_checklists_id = $newTitleChecklist->id;
-                    $newChecklist->created_at = Carbon::now();
+                    // Salin Ceklist
+                    $checklists = Checklists::where('title_checklists_id', $titleChecklist->id)->get();
+                    foreach ($checklists as $checklist) {
+                        $newChecklist = $checklist->replicate();
+                        $newChecklist->title_checklists_id = $newTitleChecklist->id;
+                        $newChecklist->created_at = Carbon::now();
 
-                    // Jika checkbox tidak tercentang, set nilai is_active
-                    // if (!$request->input('keep_checklists')) {
-                    //     $newChecklist->is_active = 0;
-                    // }
+                        // Jika checkbox tidak tercentang, set nilai is_active
+                        // if (!$request->input('keep_checklists')) {
+                        //     $newChecklist->is_active = 0;
+                        // }
 
-                    $newChecklist->save();
+                        $newChecklist->save();
+                    }
                 }
             }
+            // Salin Judul Ceklist
+
+
+            // Mendapatkan Data Kolom
+            $newColumn = Column::findOrFail($column_id);
+
+            // Untuk icon checklist
+            $titleChecklistID = TitleChecklists::where('cards_id', $card->id)->pluck('id');
+            $perChecklist = $request->input('keep_checklists') ? Checklists::whereIn('title_checklists_id', $titleChecklistID)->where('is_active', 1)->count() : 0;
+            $jumlahChecklist = Checklists::whereIn('title_checklists_id', $titleChecklistID)->count();
+
+            // Hitung persentase
+            $totalPercentage = !empty($perChecklist) ? round(($perChecklist / $jumlahChecklist) * 100) : 0;
+
+            // Mendapatkan tema aplikasi
+            $dataTema = ModeAplikasi::where('user_id', auth()->user()->user_id)->pluck('tema_aplikasi');
+            $result_tema = ModeAplikasi::whereIn('tema_aplikasi', $dataTema)->where('user_id', auth()->user()->user_id)->get();
+            $tema_aplikasi = $result_tema->pluck('tema_aplikasi');
+
+            // Siapkan respons
+            $responseData = [
+                'message' => 'Berhasil menyalin kartu!',
+                'new_card_id' => $newCard->id,
+
+                'checklists' => $checklists,
+
+                'column' => [
+                    'id' => $newColumn->id,
+                    'name' => $newColumn->name,
+                ],
+
+                'card' => [
+                    'id' => $newCard->id,
+                    'name' => $newCard->name,
+                    'pattern' => $newCard->pattern,
+                    'description' => $newCard->description,
+                    'position' => $newCard->position,
+                    'updateUrl' => route('perbaharuiKartu', ['card_id' => $newCard->id]),
+                    'deleteUrl' => route('hapusKartu', ['card_id' => $newCard->id]),
+                    'copyCardUrl' => route('copyCard', ['column_id' => $newColumn->id, 'id' => $newCard->id])
+                ],
+
+                'perChecklist' => $perChecklist,
+                'jumlahChecklist' => $jumlahChecklist,
+                'totalPercentage' => $totalPercentage,
+                'result_tema' => [
+                    'tema_aplikasi' => $tema_aplikasi
+                ]
+            ];
+
+            DB::commit();
+            return response()->json($responseData, 200);
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return response()->json([
+                'status' => 'error',
+                'message' => 'Error ' . $e->getMessage(),
+            ],400);
         }
-        // Salin Judul Ceklist
-
-
-        // Mendapatkan Data Kolom
-        $newColumn = Column::findOrFail($column_id);
-
-        // Untuk icon checklist
-        $titleChecklistID = TitleChecklists::where('cards_id', $card->id)->pluck('id');
-        $perChecklist = $request->input('keep_checklists') ? Checklists::whereIn('title_checklists_id', $titleChecklistID)->where('is_active', 1)->count() : 0;
-        $jumlahChecklist = Checklists::whereIn('title_checklists_id', $titleChecklistID)->count();
-
-        // Hitung persentase
-        $totalPercentage = !empty($perChecklist) ? round(($perChecklist / $jumlahChecklist) * 100) : 0;
-
-        // Mendapatkan tema aplikasi
-        $dataTema = ModeAplikasi::where('user_id', auth()->user()->user_id)->pluck('tema_aplikasi');
-        $result_tema = ModeAplikasi::whereIn('tema_aplikasi', $dataTema)->where('user_id', auth()->user()->user_id)->get();
-        $tema_aplikasi = $result_tema->pluck('tema_aplikasi');
-
-        // Siapkan respons
-        $responseData = [
-            'message' => 'Berhasil menyalin kartu!',
-            'new_card_id' => $newCard->id,
-
-            'checklists' => $checklists,
-
-            'column' => [
-                'id' => $newColumn->id,
-                'name' => $newColumn->name,
-            ],
-
-            'card' => [
-                'id' => $newCard->id,
-                'name' => $newCard->name,
-                'pattern' => $newCard->pattern,
-                'description' => $newCard->description,
-                'position' => $newCard->position,
-                'updateUrl' => route('perbaharuiKartu', ['card_id' => $newCard->id]),
-                'deleteUrl' => route('hapusKartu', ['card_id' => $newCard->id]),
-                'copyCardUrl' => route('copyCard', ['column_id' => $newColumn->id, 'id' => $newCard->id])
-            ],
-
-            'perChecklist' => $perChecklist,
-            'jumlahChecklist' => $jumlahChecklist,
-            'totalPercentage' => $totalPercentage,
-            'result_tema' => [
-                'tema_aplikasi' => $tema_aplikasi
-            ]
-        ];
-
-        return response()->json($responseData, 200);
     }
 
     // Untuk mendapatkan total ceklist
